3a5ba7bf0f72afee4e62e902e466931c18ae71d6
jukfloyd <zhuchkovav@gmail.com>
2022-07-28 19:06:08 +0300

    feat: test.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	bracketsConfig = bracketsConfig.map(item => item.join(''));
+	let found = true;
+	let replStr = '';
+	while (found) {
+		found = false;
+		bracketsConfig.forEach(item => {
+			replStr = str.replace(item,'');
+			if (replStr != str && !found) found = true;
+			str = replStr;
+		});
+	}
+	return (str == '') ? true : false;
 }
